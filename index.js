8669c7f31a181434ba3e156f59f383a125b76d37
unknown <donnymrshd.94@gmail.com>
2021-08-26 23:31:25 +0700

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json());
 const indexRouter = require('./src/routes/indexRouter');
 app.use('/api/v1', indexRouter);
 
+app.get('/test', (req, res) => {
+  res.send('success euyy');
+});
+
 app.listen(PORT, () => {
   console.log(`server is ok PORT : ${PORT}`);
 });
